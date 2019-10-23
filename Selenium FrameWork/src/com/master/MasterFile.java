5a5214079aff8dc797d9a630157547f480057259
NandaKumar <nanda.kumarkg@signify.com>
2019-10-23 17:16:21 +0200

    fixed

--- Selenium FrameWork/src/com/master/MasterFile.java
+++ Selenium FrameWork/src/com/master/MasterFile.java
@@ -3,11 +3,9 @@ package com.master;
 public class MasterFile {
   
  public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Master File Added from Git in feature branch which is specific for a feature!");
-=======
-		System.out.println("Master File Added from Git in develop branch which is main");
->>>>>>> refs/remotes/origin/develop
+
 	}
 
 }
