f1b85d86d55daa725bcd03dc4477383fc5e60e19
NandaKumarKG <32295845+NandaKumarKG@users.noreply.github.com>
2019-10-23 17:17:52 +0200

    Update MasterFile.java

--- Selenium FrameWork/src/com/master/MasterFile.java
+++ Selenium FrameWork/src/com/master/MasterFile.java
@@ -3,6 +3,7 @@ package com.master;
 public class MasterFile {
   
  public static void main(String[] args) {
+	 // Make sure that your branch name is updated here
 		System.out.println("Master File Added from Git in develop branch which is main");
 	}
 
