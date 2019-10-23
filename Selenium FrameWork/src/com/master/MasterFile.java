f34577773d4b95aab3d543c028e9db37365f09ba
NandaKumar <nanda.kumarkg@signify.com>
2019-10-23 17:20:58 +0200

    commited with a variable

--- Selenium FrameWork/src/com/master/MasterFile.java
+++ Selenium FrameWork/src/com/master/MasterFile.java
@@ -4,7 +4,8 @@ public class MasterFile {
   
  public static void main(String[] args) {
 	 // Make sure that your branch name is updated here
-		System.out.println("Master File Added from Git in develop branch which is main");
+	    String branchame="FeatureBranch01";
+		System.out.println("Master File Added from Git in "+branchame+" which is main");
 	}
 
 }
